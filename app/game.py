7ed73e8f6d2ddc2ff68be88cbe1a2087957b8178
Maciej Kwapisz <56626070+ciameksw@users.noreply.github.com>
2026-04-26 10:23:07 +0200

    Change code formating in game.py

--- app/game.py
+++ app/game.py
@@ -9,9 +9,10 @@ EMPTY = 0
 PLAYER1 = 1
 PLAYER2 = 2
 
-AXES = [(0,1), (1,0), (1,1), (1,-1)]
+AXES = [(0, 1), (1, 0), (1, 1), (1, -1)]
 
 Board = list[list[int]]
+
 
 @dataclass
 class MakeMoveResult:
@@ -19,21 +20,26 @@ class MakeMoveResult:
     row: int
     column: int
 
+
 def create_board() -> Board:
     return [[EMPTY for _ in range(COLUMNS)] for _ in range(ROWS)]
+
 
 def deep_board_copy(board: Board) -> Board:
     return [row.copy() for row in board]
 
+
 def print_board(board: Board):
     for row in board:
-        print('|', end='')
+        print("|", end="")
         for cell in row:
-            print(cell, '|', end='', sep='')
-        print('')
+            print(cell, "|", end="", sep="")
+        print("")
+
 
 def available_moves(board: Board) -> list[int]:
     return [i for i, cell in enumerate(board[0]) if cell == EMPTY]
+
 
 def winner(make_move_result: MakeMoveResult) -> Optional[int]:
     board = make_move_result.board
@@ -47,14 +53,19 @@ def winner(make_move_result: MakeMoveResult) -> Optional[int]:
     for delta_row, delta_column in AXES:
         connected = 1
         connected += _count_consecutive_in_dir(board, row, column, delta_row, delta_column, player)
-        connected += _count_consecutive_in_dir(board, row, column, -delta_row, -delta_column, player)
+        connected += _count_consecutive_in_dir(
+            board, row, column, -delta_row, -delta_column, player
+        )
 
         if connected >= DEFAULT_WIN_LENGTH:
             return player
-    
+
     return None
 
-def _count_consecutive_in_dir(board: Board, row: int, col: int, delta_row: int, delta_column: int, player: int) -> int:
+
+def _count_consecutive_in_dir(
+    board: Board, row: int, col: int, delta_row: int, delta_column: int, player: int
+) -> int:
     count = 0
     r = row + delta_row
     c = col + delta_column
@@ -66,8 +77,10 @@ def _count_consecutive_in_dir(board: Board, row: int, col: int, delta_row: int, 
 
     return count
 
+
 def is_final(make_move_result: MakeMoveResult) -> bool:
     return winner(make_move_result) is not None or board_is_full(make_move_result.board)
+
 
 def board_is_full(board: Board) -> bool:
     for row in board:
@@ -75,6 +88,7 @@ def board_is_full(board: Board) -> bool:
             if cell == EMPTY:
                 return False
     return True
+
 
 def next_player(board: Board) -> int:
     player1_count = 0
@@ -88,10 +102,11 @@ def next_player(board: Board) -> int:
                 player2_count += 1
     return PLAYER1 if player1_count == player2_count else PLAYER2
 
+
 def make_move(board: Board, move: int, player: int) -> MakeMoveResult:
     if not (0 <= move < COLUMNS):
         raise ValueError(f"Invalid column: {move}")
-    
+
     new_board = deep_board_copy(board)
 
     for row_idx in range(ROWS - 1, -1, -1):
