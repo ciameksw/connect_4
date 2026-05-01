1343828f2d8c2279b9aa17d2641b415001cc7a18
Maciej Kwapisz <56626070+ciameksw@users.noreply.github.com>
2026-05-01 11:35:04 +0200

    Delete ui.py

--- app/ui.py
+++
@@ -1,9 +0,0 @@
-from game_logic import Board
-
-
-def print_board(board: Board):
-    for row in board:
-        print("|", end="")
-        for cell in row:
-            print(cell, "|", end="", sep="")
-        print("")
